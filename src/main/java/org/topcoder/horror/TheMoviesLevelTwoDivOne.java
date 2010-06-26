7ac347f70c08759d956c26da46260442e7cc73dd
Miguel Covarrubias <mlcovarrubias@gmail.com>
2010-06-26 16:06:59 -0400

    real initial commit

---
+++ src/main/java/org/topcoder/horror/TheMoviesLevelTwoDivOne.java
@@ -0,0 +1,13 @@
+package org.topcoder.horror;
+
+/**
+ * Hello world!
+ *
+ */
+public class TheMoviesLevelTwoDivOne 
+{
+    
+    public int [] find(int [] length, int [] scary) {
+      return null;
+    }
+}
